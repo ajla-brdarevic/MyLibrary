4e565591c126adb14a8b6baf44a5b3f2f92d1871
Ajla Brdarević <93090001+ajla-brdarevic@users.noreply.github.com>
2023-11-15 13:26:25 +0100

    backend setup

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const cors = require('cors')
+
+const app = express()
+
+const port = 8000
+app.listen(port, () => console.log(`Server is running on port ${port}`))
